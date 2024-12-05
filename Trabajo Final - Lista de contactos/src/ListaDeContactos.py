7d48166412f84bee5e01f9fee0e8790b41e8b14c
CamiloParraS <juanparracsfn@gmail.com>
2024-12-04 19:03:01 -0500

    "Updated import statement for Contacto class"

--- Trabajo Final - Lista de contactos/src/ListaDeContactos.py
+++ Trabajo Final - Lista de contactos/src/ListaDeContactos.py
@@ -3,7 +3,7 @@ __license__="GPL"
 __version__="1.0.0"
 __Email__="juan.parrasan@campusucc.edu.co"
 
-from Mundo.Contacto import Contacto
+from Contacto import Contacto
 
 class ListaDeContactos:
     def __init__(self):
